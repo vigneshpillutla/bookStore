97e32283253c1413a8d06d6d097ea75906e5f321
Vignesh <vigneshpillutla@gmail.com>
2021-09-21 18:49:35 +0530

    Added browse genres

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from 'react';
 import useAuth from 'customHooks/useAuth';
 import { Link, useHistory } from 'react-router-dom';
 import { Grid, makeStyles, Typography, Button } from '@material-ui/core';
-import BookReader from '../../media/bookReader1.svg';
+import Hero from './Hero';
+import BrowseGenres from './BrowseGenres';
+
 const useStyles = makeStyles((theme) => ({
   root: {
     height: '100%'
@@ -23,6 +25,7 @@ const useStyles = makeStyles((theme) => ({
     fontStyle: 'italic'
   }
 }));
+
 const HomePage = () => {
   const auth = useAuth();
   const history = useHistory();
@@ -34,39 +37,8 @@ const HomePage = () => {
   };
   return (
     <div>
-      <Grid container className={classes.heroGrid}>
-        <Grid item xs={1} md={2}></Grid>
-        <Grid
-          container
-          item
-          xs={10}
-          md={10}
-          classes={{ item: classes.heroGridItem }}
-        >
-          <Grid item container sm={12} md={6} direction="column" spacing={5}>
-            <Grid item data-aos="fade-right">
-              <Typography variant="h2">Find your book</Typography>
-              <Typography variant="h2">& lose yourself</Typography>
-              <Typography
-                variant="subtitle1"
-                component="p"
-                className={classes.subtitleStyle}
-              >
-                Read all your favourite fan fictions, romance novels or your
-                academic books. Pen down your creative ideas all in here.
-              </Typography>
-            </Grid>
-            <Grid item data-aos="fade-right">
-              <Button variant="contained" color="secondary" size="large">
-                Explore
-              </Button>
-            </Grid>
-          </Grid>
-          <Grid item sm={12} md={6} data-aos="fade-left">
-            <img className={classes.heroImage} src={BookReader} alt="" />
-          </Grid>
-        </Grid>
-      </Grid>
+      <Hero />
+      <BrowseGenres />
     </div>
   );
 };
