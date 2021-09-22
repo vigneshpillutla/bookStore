249573acb96448ab6139ed904eddef18d4480096
Vignesh <vigneshpillutla@gmail.com>
2021-09-23 02:23:36 +0530

    Added view of a particular book

---
+++ src/components/Home/BookShowCase.jsx
@@ -0,0 +1,129 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import Slider from 'react-slick';
+import {
+  makeStyles,
+  Card,
+  CardMedia,
+  Grid,
+  Typography,
+  CardContent,
+  Button
+} from '@material-ui/core';
+import { ArrowForwardIos, ArrowBackIos } from '@material-ui/icons';
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: '5rem',
+    marginBottom: '5rem'
+  },
+  cardStyle: {
+    height: '100%',
+    background: 'gray',
+    marginRight: '20px',
+    position: 'relative',
+    cursor: 'pointer',
+    '& > *': {
+      transitionDuration: '0.3s'
+    },
+    '&:hover': {
+      '& > :last-child': {
+        opacity: 1
+      }
+    }
+  },
+  cardContainer: {
+    width: 200,
+    height: 200,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  arrowStyle: {
+    color: '#000',
+    '&:hover': {
+      color: 'gray'
+    }
+  },
+  media: {
+    height: '100%'
+  },
+  cardContent: {
+    position: 'absolute',
+    width: '100%',
+    top: 0,
+    height: '100%',
+    backgroundColor: 'rgba(255,255,255,0.5)',
+    opacity: 0,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+    '& > button': {
+      whiteSpace: 'nowrap'
+    }
+  },
+  title: {
+    cursor: 'pointer',
+    '&:hover': {
+      color: '#2f70c4'
+    }
+  }
+}));
+const BookShowCase = ({ title, getData, filters, ignoreId }) => {
+  const classes = useStyles();
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    getData((books) => {
+      books = books?.filter((book) => book.bookId !== ignoreId);
+      setData(books);
+    }, filters);
+  }, [filters]);
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 7,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    pauseOnHover: true,
+    nextArrow: <ArrowForwardIos classes={{ root: classes.arrowStyle }} />,
+    prevArrow: <ArrowBackIos classes={{ root: classes.arrowStyle }} />
+  };
+  if (!data) {
+    return <div></div>;
+  }
+  return (
+    <div className={classes.root} data-aos="fade-up">
+      <Grid container>
+        <Grid item xs={1}></Grid>
+        <Grid item container xs={11} spacing={4}>
+          <Grid item xs={12}>
+            <Typography variant="h4" className={classes.title}>
+              {title}
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Slider {...settings}>
+              {data.map(({ bookCover, bookId }) => (
+                <div className={classes.cardContainer}>
+                  <Card elevation={3} className={classes.cardStyle}>
+                    <CardMedia image={bookCover} className={classes.media} />
+                    <CardContent className={classes.cardContent}>
+                      <Link to={`/books/view/${bookId}`}>
+                        <Button variant="contained" color="primary">
+                          View More
+                        </Button>
+                      </Link>
+                    </CardContent>
+                  </Card>
+                </div>
+              ))}
+            </Slider>
+          </Grid>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default BookShowCase;
---
+++ src/components/ViewBook/ViewBook.jsx
@@ -0,0 +1,163 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import BookLibrary from 'common/bookUtil';
+import BookShowCase from 'components/Home/BookShowCase';
+import {
+  makeStyles,
+  Grid,
+  Typography,
+  Button,
+  Card,
+  CardContent,
+  Backdrop,
+  CircularProgress
+} from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: '3.5em'
+  },
+  coverContainer: {
+    display: 'flex',
+    '& > img': {
+      width: 200,
+      height: 300
+    }
+  },
+  actions: {
+    display: 'flex',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  details: {
+    textAlign: 'center',
+    color: 'rgba(0,0,0,0.65)'
+  },
+  bookTitle: {
+    fontStyle: 'italic',
+    color: 'rgba(0,0,0,0.7)'
+  },
+  bookExcerpt: {
+    color: 'rgba(0,0,0,0.7)'
+  },
+  moreLikeThis: {
+    textAlign: 'center',
+    marginTop: '4rem'
+  }
+}));
+const ViewBook = () => {
+  const library = new BookLibrary();
+  const { bookId } = useParams();
+  const [book, setBook] = useState({});
+  const [filters, setFilters] = useState([]);
+  const classes = useStyles();
+  useEffect(() => {
+    library.getSingleBook(bookId, (bookData) => {
+      setBook(bookData);
+      const filters = bookData.genres?.map((genre) => ['genres', genre]);
+      setFilters(filters);
+      console.log(bookData);
+    });
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }, [bookId]);
+  const Description = () => {
+    const { description } = book;
+
+    const GridItem = (
+      <Grid item xs={12}>
+        <Card elevation={3}>
+          <CardContent>
+            <Typography variant="h5" gutterBottom>
+              Description
+            </Typography>
+            <Typography variant="body2" component="p">
+              <i>{book.description}</i>
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    );
+
+    return !!description ? GridItem : null;
+  };
+  const Excerpt = () => {
+    const { excerpt } = book;
+
+    const GridItem = (
+      <Grid item xs={12}>
+        <Card elevation={3}>
+          <CardContent>
+            <Typography
+              variant="h6"
+              gutterBottom
+              className={classes.bookExcerpt}
+            >
+              Book Excerpt
+            </Typography>
+            <Typography variant="body2" component="p">
+              {book.excerpt}
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    );
+
+    return !!excerpt ? GridItem : null;
+  };
+
+  return (
+    <div className={classes.root}>
+      <Grid container>
+        <Grid item xs={1}></Grid>
+        <Grid item container xs={10}>
+          <Grid item container xs={4} spacing={4}>
+            <Grid
+              item
+              xs={12}
+              className={classes.coverContainer}
+              justifyContent="center"
+            >
+              <img src={book.bookCover} alt="bookCover" />
+            </Grid>
+            <Grid item xs={12} className={classes.details}>
+              <Typography>Pages: {book.pages}</Typography>
+              <Typography>Rating: {book.rating} / 5</Typography>
+            </Grid>
+            <Grid item xs={12} className={classes.actions}>
+              <Button variant="outlined" color="secondary">
+                Add to cart
+              </Button>
+              <Button variant="outlined" color="secondary">
+                Read Now
+              </Button>
+            </Grid>
+          </Grid>
+          <Grid item container xs={8} spacing={4}>
+            <Grid item className={classes.title} xs={12}>
+              <Typography variant="h4">{book.name}</Typography>
+              <Typography variant="subtitle1" className={classes.bookTitle}>
+                by {book.author}
+              </Typography>
+            </Grid>
+            <Description />
+            <Excerpt />
+          </Grid>
+          <Grid item xs={12} className={classes.moreLikeThis}>
+            <Typography variant="h4">More Like This</Typography>
+            <BookShowCase
+              getData={library.getBooksByFilter}
+              filters={filters}
+              ignoreId={book.bookId}
+            />
+          </Grid>
+        </Grid>
+        <Grid item xs={1}></Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default ViewBook;
