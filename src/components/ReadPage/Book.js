d44829a0f077c68b43f46f61321aa0bca8207de6
ManasRaut <msraut2002@gmail.com>
2021-10-22 10:09:06 +0530

    added infobox and changed readPage ui

--- src/components/ReadPage/Book.js
+++ src/components/ReadPage/Book.js
@@ -1,128 +1,134 @@
-import react, { useCallback, useEffect, useRef, useState } from 'react';
+import react, { useCallback, useEffect, useRef, useState } from "react";
 
 export const Book = (props) => {
-  const [pagesData, setPagesData] = useState([]);
+    const [pagesData, setPagesData] = useState([]);
 
-  const getPage = async (url) => {
-    const response = await fetch(url);
-    const json = await response.json();
-    return json;
-  };
+    const getPage = async (url) => {
+        const response = await fetch(url);
+        const json = await response.json();
+        return json;
+    };
 
-  // useEffect(() => {
-  //     if (props.isDataFetched) {
-  //         const newData = [];
-  //         for (let i = 0; i < props.pages; i++) {
-  //             newData.push({
-  //                 id: "",
-  //                 html: "",
-  //                 target: "",
-  //             });
-  //         }
-  //         for (let i = 1; i <= props.pages; i++) {
-  //             const url =
-  //                 "http://localhost:3001/read?n=" +
-  //                 props.bookName +
-  //                 "&p=" +
-  //                 i;
-  //             // const url =
-  //             //     "http://localhost:5000/api/readBook/read?n=" +
-  //             //     props.bookName +
-  //             //     "&p=" +
-  //             //     i;
-  //             (async () => {
-  //                 const json = await getPage(url);
-  //                 newData[json.pageNo - 1].id = json.pageNo;
-  //                 newData[json.pageNo - 1].html = json.data;
-  //                 newData[json.pageNo - 1].target = json.target;
+    // useEffect(() => {
+    //     if (props.isDataFetched) {
+    //         const newData = [];
+    //         for (let i = 0; i < props.pages; i++) {
+    //             newData.push({
+    //                 id: "",
+    //                 html: "",
+    //                 target: "",
+    //             });
+    //         }
+    //         for (let i = 1; i <= props.pages; i++) {
+    //             const url =
+    //                 "http://localhost:3001/read?n=" +
+    //                 props.bookName +
+    //                 "&p=" +
+    //                 i;
+    //             // const url =
+    //             //     "http://localhost:5000/api/readBook/read?n=" +
+    //             //     props.bookName +
+    //             //     "&p=" +
+    //             //     i;
+    //             (async () => {
+    //                 const json = await getPage(url);
+    //                 newData[json.pageNo - 1].id = json.pageNo;
+    //                 newData[json.pageNo - 1].html = json.data;
+    //                 newData[json.pageNo - 1].target = json.target;
 
-  //                 if (i == props.pages) {
-  //                     setPagesData(newData);
-  //                 }
-  //             })();
-  //         }
-  //     }
-  //     console.log(props.isDataFetched);
-  // }, [props.isDataFetched]);
+    //                 if (i == props.pages) {
+    //                     setPagesData(newData);
+    //                 }
+    //             })();
+    //         }
+    //     }
+    //     console.log(props.isDataFetched);
+    // }, [props.isDataFetched]);
 
-  return (
-    <>
-      <div className="book-cont">
-        <div className="book-title">{props.bookName}</div>
-        {pagesData.map((page) => {
-          return (
-            <Page
-              key={page.id}
-              id={page.target}
-              html={page.html}
-              isBookmarking={props.isBookmarking}
-              isHighlighting={props.isHighlighting}
-            />
-          );
-        })}
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className='book-cont'>
+                <div className='book-title'>{props.bookName}</div>
+                {pagesData.map((page) => {
+                    return (
+                        <Page
+                            key={page.id}
+                            id={page.target}
+                            html={page.html}
+                            isBookmarking={props.isBookmarking}
+                            isHighlighting={props.isHighlighting}
+                        />
+                    );
+                })}
+            </div>
+        </>
+    );
 };
 
 const saveHighlight = (ref, id) => {
-  const selection = ref.current.contentDocument.getSelection();
-  console.log(selection);
-  const start = selection.anchorOffset;
-  const l = selection.toString().length;
-  const first = selection.anchorNode.parentNode.innerHTML.slice(0, start);
-  const second = selection.anchorNode.parentNode.innerHTML.slice(start + l);
-  const r =
-    "<span style='background-color:tomato;'>" +
-    selection.anchorNode.parentNode.innerHTML.slice(start, start + l) +
-    '</span>';
-  selection.anchorNode.parentNode.innerHTML = first + r + second;
+    const selection = ref.current.contentDocument.getSelection();
+    console.log(selection);
+    const start = selection.anchorOffset;
+    const l = selection.toString().length;
+    const first = selection.anchorNode.parentNode.innerHTML.slice(0, start);
+    const second = selection.anchorNode.parentNode.innerHTML.slice(start + l);
+    const r =
+        "<span style='background-color:tomato;'>" +
+        selection.anchorNode.parentNode.innerHTML.slice(start, start + l) +
+        "</span>";
+    selection.anchorNode.parentNode.innerHTML = first + r + second;
 };
 
 const Page = (props) => {
-  const refw = useRef(null);
+    const refw = useRef(null);
 
-  const addBookMark = useCallback(() => {
-    console.log(props.id);
-  }, []);
-  const stopHighlight = useCallback(() => {
-    saveHighlight(refw, props.id);
-  }, []);
+    const addBookMark = useCallback(() => {
+        console.log(props.id);
+    }, []);
+    const stopHighlight = useCallback(() => {
+        saveHighlight(refw, props.id);
+    }, []);
 
-  useEffect(() => {
-    refw.current.addEventListener('load', () => {
-      refw.current.height =
-        refw.current.contentDocument.documentElement.scrollHeight;
-    });
-  }, []);
+    useEffect(() => {
+        refw.current.addEventListener("load", () => {
+            refw.current.height =
+                refw.current.contentDocument.documentElement.scrollHeight;
+        });
+    }, []);
 
-  useEffect(() => {
-    if (props.isBookmarking) {
-      refw.current.contentDocument.addEventListener('click', addBookMark);
-    } else {
-      refw.current.contentDocument.removeEventListener('click', addBookMark);
-    }
-  }, [props.isBookmarking]);
+    useEffect(() => {
+        if (props.isBookmarking) {
+            refw.current.contentDocument.addEventListener("click", addBookMark);
+        } else {
+            refw.current.contentDocument.removeEventListener(
+                "click",
+                addBookMark
+            );
+        }
+    }, [props.isBookmarking]);
 
-  useEffect(() => {
-    if (props.isHighlighting) {
-      refw.current.contentDocument.addEventListener('mouseup', stopHighlight);
-    } else {
-      refw.current.contentDocument.removeEventListener(
-        'mouseup',
-        stopHighlight
-      );
-    }
-  }, [props.isHighlighting]);
+    useEffect(() => {
+        if (props.isHighlighting) {
+            refw.current.contentDocument.addEventListener(
+                "mouseup",
+                stopHighlight
+            );
+        } else {
+            refw.current.contentDocument.removeEventListener(
+                "mouseup",
+                stopHighlight
+            );
+        }
+    }, [props.isHighlighting]);
 
-  return (
-    <>
-      <iframe
-        id={props.id}
-        className="book-page"
-        ref={refw}
-        srcDoc={props.html}
-      ></iframe>
-    </>
-  );
+    return (
+        <>
+            <iframe
+                id={props.id}
+                className='book-page'
+                ref={refw}
+                srcDoc={props.html}
+            ></iframe>
+        </>
+    );
 };
--- src/components/ReadPage/ReadPage.js
+++ src/components/ReadPage/ReadPage.js
@@ -1,161 +1,293 @@
-import react, { useEffect, useState } from 'react';
-import './ReadPage.css';
-import { Book } from './Book';
-import axios from 'axios';
-import keys from 'configs/keys.js';
-import { axiosCng } from 'common';
-import { useParams, useHistory } from 'react-router-dom';
+import react, { useEffect, useState } from "react";
+import "./ReadPage.css";
+import { Book } from "./Book";
+import BookLibrary from "common/bookUtil";
+import axios from "axios";
+import keys from "configs/keys.js";
+import { axiosCng } from "common";
+import { useParams, useHistory } from "react-router-dom";
+import Rating from "@material-ui/lab/Rating";
+import BookmarkBorderRoundedIcon from "@material-ui/icons/BookmarkBorderRounded";
+import ListRoundedIcon from "@material-ui/icons/ListRounded";
+import CloseRoundedIcon from "@material-ui/icons/CloseRounded";
+import InfoRoundedIcon from "@material-ui/icons/InfoRounded";
+import BrushRoundedIcon from "@material-ui/icons/BrushRounded";
+import AddRoundedIcon from "@material-ui/icons/AddRounded";
+import DeleteRoundedIcon from "@material-ui/icons/DeleteRounded";
+
 const serverPath = keys.serverDomain;
 const bookDetailsUrl = `${serverPath}/api/readBook`;
 
-let htmlData;
-
 const ReadPage = (props) => {
-  const [contentIndex, setContentIndex] = useState([]);
-  const [bookInfo, setBookInfo] = useState({ name: 'unknown', pages: -1 });
-  const [isDataFetched, setIsDataFetched] = useState(false);
-  const [isBookmarking, setIsBookMarking] = useState(false);
-  const [showIndexBar, setShowIndexBar] = useState(false);
-  const [isHighlighting, setIsHighlighting] = useState(false);
-  const [isPotrait, setIsPotrait] = useState(true);
-  const toggleBookMark = () => setIsBookMarking(!isBookmarking);
-  const history = useHistory();
-  let { bookId } = useParams();
+    const [contentIndex, setContentIndex] = useState([]);
+    const [bookInfo, setBookInfo] = useState({ name: "unknown", pages: -1 });
+    const [isDataFetched, setIsDataFetched] = useState(false);
+    const [isBookmarking, setIsBookMarking] = useState(false);
+    const [showIndexBar, setShowIndexBar] = useState(false);
+    const [showInfo, setShowInfo] = useState(false);
+    const [showBookMCont, setShowBookMCont] = useState(false);
+    const [isHighlighting, setIsHighlighting] = useState(false);
+    const [isPotrait, setIsPotrait] = useState(true);
+    const [bookdata, setBookdata] = useState({
+        bookId: null,
+        bookName: null,
+        author: null,
+        pages: null,
+        rating: null,
+        genres: [],
+    });
+    const toggleBookMark = () => setIsBookMarking(!isBookmarking);
+    const history = useHistory();
+    let { bookId } = useParams();
 
-  useEffect(() => {
-    const getDetails = async () => {
-      const res = await axios.get(`${bookDetailsUrl}/getBookDetails`, {
-        params: {
-          bookId
-        },
-        ...axiosCng,
-        withCredentials: true
-      });
-      const { success } = res.data;
-      if (!success) {
-        history.push('/');
-        return;
-      }
-      const { data } = res.data;
-      setContentIndex(data.contentsGuide);
-      setBookInfo({ name: data.bookName, pages: data.pages });
-      setIsDataFetched(true);
-    };
-    getDetails();
-    // const url = 'http://localhost:3001/getBookDetails?n=' + props.bookName;
-    // fetch(url)
-    //   .then((res) => {
-    //     return res.json();
-    //   })
-    //   .then((res) => {
-    //     setContentIndex(res.data.contentsGuide);
-    //     setBookInfo({ name: res.data.bookName, pages: res.data.pages });
-    //     setIsDataFetched(true);
-    //   });
-  }, [bookId]);
-  return (
-    <>
-      <ToolBar
-        toggleBookMark={toggleBookMark}
-        showIndexBar={showIndexBar}
-        setShowIndexBar={setShowIndexBar}
-        isHighlighting={isHighlighting}
-        setIsHighlighting={setIsHighlighting}
-        isPotrait={isPotrait}
-        setIsPotrait={setIsPotrait}
-      />
-      <Book
-        bookName={bookInfo.name}
-        pages={bookInfo.pages}
-        isDataFetched={isDataFetched}
-        isBookmarking={isBookmarking}
-        isHighlighting={isHighlighting}
-        isPotrait={isPotrait}
-        setIsPotrait={setIsPotrait}
-      />
-      <Index
-        contentIndex={contentIndex}
-        showIndexBar={showIndexBar}
-        setShowIndexBar={setShowIndexBar}
-      />
-    </>
-  );
+    const library = new BookLibrary();
+
+    useEffect(() => {
+        const getDetails = async () => {
+            const res = await axios.get(`${bookDetailsUrl}/getBookDetails`, {
+                params: {
+                    bookId,
+                },
+                ...axiosCng,
+                withCredentials: true,
+            });
+            const { success } = res.data;
+            if (!success) {
+                history.push("/");
+                return;
+            }
+            const { data } = res.data;
+            setContentIndex(data.contentsGuide);
+            setBookInfo({ name: data.bookName, pages: data.pages });
+            setIsDataFetched(true);
+        };
+        getDetails();
+        library.getSingleBook(bookId, (bookData) => {
+            console.log(bookData);
+            setBookdata({
+                bookId: bookData.bookId,
+                bookName: bookData.name,
+                author: bookData.author,
+                pages: bookData.pages,
+                rating: bookData.rating,
+                genres: bookData.genres,
+            });
+        });
+        // const url = 'http://localhost:3001/getBookDetails?n=' + props.bookName;
+        // fetch(url)
+        //   .then((res) => {
+        //     return res.json();
+        //   })
+        //   .then((res) => {
+        //     setContentIndex(res.data.contentsGuide);
+        //     setBookInfo({ name: res.data.bookName, pages: res.data.pages });
+        //     setIsDataFetched(true);
+        //   });
+    }, [bookId]);
+    return (
+        <div style={{ cursor: isBookmarking ? "grab" : "default" }}>
+            <ToolBar
+                showIndexBar={showIndexBar}
+                setShowIndexBar={setShowIndexBar}
+                showInfo={showInfo}
+                setShowInfo={setShowInfo}
+                isHighlighting={isHighlighting}
+                setIsHighlighting={setIsHighlighting}
+                isPotrait={isPotrait}
+                setIsPotrait={setIsPotrait}
+                showBookMCont={showBookMCont}
+                setShowBookMCont={setShowBookMCont}
+            />
+            <Book
+                bookName={bookdata.bookName}
+                pages={bookdata.pages}
+                isDataFetched={isDataFetched}
+                isBookmarking={isBookmarking}
+                isHighlighting={isHighlighting}
+                isPotrait={isPotrait}
+                setIsPotrait={setIsPotrait}
+            />
+            <Index
+                contentIndex={contentIndex}
+                showIndexBar={showIndexBar}
+                setShowIndexBar={setShowIndexBar}
+            />
+            <Info
+                showInfo={showInfo}
+                setShowInfo={setShowInfo}
+                bookdata={bookdata}
+            />
+            <BookMarkCont
+                showBookMCont={showBookMCont}
+                toggleBookMark={toggleBookMark}
+            />
+        </div>
+    );
 };
 
 const ToolBar = (props) => {
-  return (
-    <>
-      <div className="toolbar-cont">
-        <button
-          className="tool-btn"
-          onClick={() => props.setShowIndexBar(!props.showIndexBar)}
-        >
-          I
-        </button>
-        <button className="tool-btn" onClick={() => props.toggleBookMark()}>
-          B
-        </button>
-        <button
-          className="tool-btn"
-          onClick={() => props.setIsHighlighting(!props.isHighlighting)}
-        >
-          H
-        </button>
-        <button
-          className="tool-btn"
-          onClick={() => props.setIsPotrait(!props.isPotrait)}
-        >
-          IF
-        </button>
-        <button className="tool-btn">DF</button>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className='toolbar-cont'>
+                <button
+                    className='tool-btn'
+                    onClick={() => {
+                        props.setShowIndexBar(!props.showIndexBar);
+                        props.setShowInfo(false);
+                    }}
+                >
+                    <ListRoundedIcon />
+                </button>
+                <button
+                    className='tool-btn'
+                    onClick={() => props.setShowBookMCont(!props.showBookMCont)}
+                >
+                    <BookmarkBorderRoundedIcon />
+                </button>
+                <button
+                    className='tool-btn'
+                    onClick={() =>
+                        props.setIsHighlighting(!props.isHighlighting)
+                    }
+                >
+                    <BrushRoundedIcon />
+                </button>
+                <button
+                    className='tool-btn'
+                    onClick={() => {
+                        props.setShowInfo(!props.showInfo);
+                        props.setShowIndexBar(false);
+                    }}
+                >
+                    <InfoRoundedIcon />
+                </button>
+            </div>
+        </>
+    );
 };
 const Index = (props) => {
-  return (
-    <div
-      className="index-cont"
-      style={{ right: props.showIndexBar ? '10px' : '-250px' }}
-    >
-      <div className="title-cont">
-        <div className="title">Contents</div>
-        <button
-          className="close-btn"
-          onClick={() => props.setShowIndexBar(false)}
+    return (
+        <div
+            className='index-cont elevated'
+            style={{ right: props.showIndexBar ? "20px" : "-250px" }}
         >
-          X
-        </button>
-      </div>
+            <div className='title-cont'>
+                <div className='title'>Contents</div>
+                <button
+                    className='close-btn'
+                    onClick={() => props.setShowIndexBar(false)}
+                >
+                    <CloseRoundedIcon />
+                </button>
+            </div>
 
-      <div className="contents-list">
-        {props.contentIndex.map((item) => {
-          return <Item title={item.title} target={item.target} />;
-        })}
-      </div>
-    </div>
-  );
+            <div className='contents-list'>
+                {props.contentIndex.map((item) => {
+                    return <Item title={item.title} target={item.target} />;
+                })}
+            </div>
+        </div>
+    );
 };
 
 const Item = (props) => {
-  return (
-    <>
-      <div
-        className="content-item"
-        onClick={() => {
-          if (props.target.search(/.html#/) == -1) {
-            window.document.getElementById(props.target).scrollIntoView();
-          } else {
-            window.document
-              .getElementById(props.target.slice(0, props.target.search('#')))
-              .scrollIntoView();
-          }
-        }}
-      >
-        {props.title}
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div
+                className='content-item'
+                onClick={() => {
+                    if (props.target.search(/.html#/) == -1) {
+                        window.document
+                            .getElementById(props.target)
+                            .scrollIntoView();
+                    } else {
+                        window.document
+                            .getElementById(
+                                props.target.slice(0, props.target.search("#"))
+                            )
+                            .scrollIntoView();
+                    }
+                }}
+            >
+                {props.title}
+            </div>
+        </>
+    );
 };
 
+const Info = (props) => {
+    return (
+        <div
+            style={{ right: props.showInfo ? "10px" : "-350px" }}
+            className='info-cont elevated'
+        >
+            <div className='info-title-cont'>
+                <button
+                    className='close-btn'
+                    style={{ height: "30px" }}
+                    onClick={() => props.setShowInfo(!props.showInfo)}
+                >
+                    <CloseRoundedIcon />
+                </button>
+            </div>
+            <div className='main-info-cont'>
+                <div className='info-bookname'>{props.bookdata.bookName}</div>
+                <div>Author:{props.bookdata.author}</div>
+                <div>Total pages: {props.bookdata.pages}</div>
+                <div>
+                    <Rating
+                        name='read-only'
+                        value={props.bookdata.rating}
+                        precision={0.2}
+                        readOnly
+                    />
+                </div>
+                <div className='genres-cont'>
+                    {props.bookdata.genres.map((genre) => {
+                        return <GenreBadge genre={genre} />;
+                    })}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+const GenreBadge = (props) => {
+    return (
+        <div className='genre-badge'>
+            <div></div>
+            {props.genre}
+        </div>
+    );
+};
+const bookmarks = [
+    { id: 0, name: "Chapter 1", target: "none" },
+    { id: 1, name: "Chapter 1", target: "none" },
+];
+const BookMarkCont = (props) => {
+    return (
+        <div
+            className='bookmark-cont elevated'
+            style={{ display: props.showBookMCont ? "flex" : "none" }}
+        >
+            {bookmarks.map((bk) => {
+                return (
+                    <div className='bkmrk-item' key={bk.id}>
+                        {bk.name}
+                        <div className='content-item delete-btn'>
+                            <DeleteRoundedIcon />
+                        </div>
+                    </div>
+                );
+            })}
+
+            <div
+                className='content-item add-btn'
+                onClick={() => props.toggleBookMark()}
+            >
+                <AddRoundedIcon />
+            </div>
+        </div>
+    );
+};
 export default ReadPage;
